658e131cca04bf4f7d9ba24362fef25d689c899f
JoseEduardoR <40973882+JoseEduardoR@users.noreply.github.com>
2021-09-30 13:53:31 -0500

    estilo en botones

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,8 +1,13 @@
 <x-app-layout>
     <div class="max-w-6xl mx-auto bg-indigo-500 p-3">
-            <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+<!--            <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
                 Nuevo
+            </button>-->
+        
+             <button type="button" title="Create a product" class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded">
+                    <i class="fas fa-plus-circle"></i>
             </button>
+        
         <div class="bg-white shadow-md rounded my-6">
         <table class="text-left w-full border-collapse">
             <thead>
@@ -33,7 +38,7 @@
                     <td class="py-4 px-6 border-b border-grey-light">
                         {{$user->email_verified_at}}
                     </td>
-                    <td>
+<!--                    <td>
                         <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
                             Ver
                         </button>
@@ -42,11 +47,28 @@
                         <button class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
                             Editar
                         </button>
-                    </td>
+                    </td>-->
                     <td>
-                        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+<!--                        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
                             Borrar
+                        </button>-->
+                    <form action="" method="POST">
+
+                        <button type="button" title="edit" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                                <i class="fas fa-eye text-success  fa-lg"></i>
                         </button>
+
+                        <button type="button" title="edit" class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
+                                <i class="fas fa-edit  fa-lg"></i>
+                        </button>
+
+                        @csrf
+                        @method('DELETE')
+
+                        <button type="submit" title="delete" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                            <i class="fas fa-trash fa-lg text-danger"></i>
+                        </button>
+                    </form>
                     </td>
                 </tr>
             @endforeach
